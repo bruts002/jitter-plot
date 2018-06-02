e813670a681d71e1e74ef82e65197ebfe1f8c1be
Michael Brutskiy <mikebrutskiy@gmail.com>
2018-06-02 13:21:10 -0500

    Calculates widths ahead of time

--- src/inputs/RangeInput/RangeInput.js
+++ src/inputs/RangeInput/RangeInput.js
@@ -8,44 +8,50 @@ export default ({
     onChange,
     step = Math.round((max - min) / 10),
     resizerWidth = 10
-}) => <div className="range__container">
-    <div className="range__inputs">
-        <input
-            type='number'
-            onChange={ e => onChange({
-                lowerBound: +e.target.value,
-                upperBound
-            })}
-            step={step}
-            value={lowerBound}
-            max={Math.min(max, upperBound)}
-            min={min}/>
-        <input
-            type='number'
-            onChange={ e => onChange({
-                upperBound: +e.target.value,
-                lowerBound
-            })}
-            step={step}
-            value={upperBound}
-            max={max}
-            min={Math.max(min, lowerBound)}/>
+}) => {
+    const scale = 100/(max-min);
+    const startWidth = (lowerBound-min)*scale;
+    const endWidth = (max-upperBound)*scale;
+    const middleWidth = (upperBound-lowerBound)*scale;
+    return <div className="range__container">
+        <div className="range__inputs">
+            <input
+                type='number'
+                onChange={ e => onChange({
+                    lowerBound: +e.target.value,
+                    upperBound
+                })}
+                step={step}
+                value={lowerBound}
+                max={Math.min(max, upperBound)}
+                min={min}/>
+            <input
+                type='number'
+                onChange={ e => onChange({
+                    upperBound: +e.target.value,
+                    lowerBound
+                })}
+                step={step}
+                value={upperBound}
+                max={max}
+                min={Math.max(min, lowerBound)}/>
+        </div>
+        <div className='range__slider'>
+            <div
+                className='range__chunk range__chunk--start'
+                style={{flexBasis: `${startWidth}%`}}></div>
+            <div
+                className='range__control'
+                style={{flexBasis: `${resizerWidth}px`}}></div>
+            <div
+                className='range__chunk range__chunk--middle'
+                style={{flexBasis: `calc(${middleWidth}% - ${2*resizerWidth}px`}}></div>
+            <div
+                className='range__control'
+                style={{flexBasis: `${resizerWidth}px`}}></div>
+            <div
+                className='range__chunk range__chunk--end'
+                style={{flexBasis: `${endWidth}%`}}></div>
+        </div>
     </div>
-    <div className='range__slider'>
-        <div
-            className='range__chunk range__chunk--start'
-            style={{flexBasis: `${((lowerBound-min)/(max-min))*100}%`}}></div>
-        <div
-            className='range__control'
-            style={{flexBasis: `${resizerWidth}px`}}></div>
-        <div
-            className='range__chunk range__chunk--middle'
-            style={{flexBasis: `calc(${((upperBound-lowerBound)/(max-min))*100}% - ${2*resizerWidth}px`}}></div>
-        <div
-            className='range__control'
-            style={{flexBasis: `${resizerWidth}px`}}></div>
-        <div
-            className='range__chunk range__chunk--end'
-            style={{flexBasis: `${((max-upperBound)/(max-min))*100}%`}}></div>
-    </div>
-</div>
+}
