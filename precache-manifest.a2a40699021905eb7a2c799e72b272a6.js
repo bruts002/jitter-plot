d5a4f6c12ee3fc2310c3cb48a7a5b3ae2872ff08
bruts002 <bruts002@users.noreply.github.com>
2020-03-14 18:20:30 +0000

    deploy: 10d1bd301934a3e628e21a6020aec8d031fe3b9a

---
+++ precache-manifest.a2a40699021905eb7a2c799e72b272a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30eccc7df38f6a40b62ae1107094825f",
+    "url": "/jitter-plot/index.html"
+  },
+  {
+    "revision": "0606c44616ab4e18c651",
+    "url": "/jitter-plot/static/css/main.1860d49f.chunk.css"
+  },
+  {
+    "revision": "ac1c2b21a321537b832d",
+    "url": "/jitter-plot/static/js/2.1a011119.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/jitter-plot/static/js/2.1a011119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0606c44616ab4e18c651",
+    "url": "/jitter-plot/static/js/main.07c30c3b.chunk.js"
+  },
+  {
+    "revision": "f94e83b18d643f71151d",
+    "url": "/jitter-plot/static/js/runtime-main.890226af.js"
+  }
+]);
